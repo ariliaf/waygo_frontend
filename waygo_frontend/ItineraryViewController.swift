ab530e16d604589c3d69c227704ee7e1fc5ef4db
Darrion Vinson <dmvinson326@gmail.com>
2019-01-10 17:36:37 -0500

    Added itineraries view

--- waygo_frontend/ItineraryViewController.swift
+++ waygo_frontend/ItineraryViewController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import Foundation
 
-class TableViewController: UITableViewController {
+
+class ItineraryViewController: UITableViewController {
+    
+    var itineraries = [Itinerary]()
+    
+    let cellID = "itineraryCell"
+    
+    @IBOutlet var dataTable: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,23 +33,31 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return itineraries.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let it = itineraries[indexPath.row]
+        
+        cell.textLabel?.text = it.name
+        if let budget = it.budget, let planDate = it.planDate {
+            let budgetStr = "$\(String(budget))"
+            let formatter = DateFormatter()
+            formatter.timeStyle = DateFormatter.Style.none
+            formatter.dateStyle = DateFormatter.Style.short
+            let dateStr = formatter.string(from: planDate)
+            cell.detailTextLabel.text = "\(dateStr) - $\(budgetStr)"
+        }
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
